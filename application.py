c2dd68c8f13168be76c14093cb6ff89a15d51447
Shuyang <l.shuyang98@gmail.com>
2020-05-29 07:43:22 +0800

    working version of home.html

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os, csv
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,20 +20,24 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+
 stops = []
 
 class Stop:
 	def __init__(self, id, name, dur):
 		self.id = id
 		self.name = name
-		self.dur=dur
+		self.dur = dur
 
 with open("stopdata.csv") as f:
 	data = csv.reader(f)
 	for row in data:
-		stops.append(Stop(row[0], row[2], row[2]))
- 
+		stops.append(Stop(row[0], row[1], row[2]))
 
 @app.route("/")
 def home():
     return render_template("home.html", stops = stops)
+
+@app.route("/direct")
+def direct():
+ 	return "hello world!!!"
