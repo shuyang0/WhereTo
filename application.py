7e6181240341e15504f91202b50ac266ed3a6c96
Shuyang <l.shuyang98@gmail.com>
2020-06-29 13:36:51 +0800

    bus stop labeling along generated route

--- application.py
+++ application.py
@@ -34,13 +34,14 @@ def home():
 def go():
 	start_id = request.form.get("start")
 	end_id = request.form.get("end")
-	start_name = stopDict[start_id]['name']
-	end_name = stopDict[end_id]['name']
 	if start_id == end_id:
-		coord = [start_name, stopDict[start_id]['lng'], stopDict[start_id]['lat']]
-		return render_template("go.html", start_name = start_name, coord = coord,  same = True)
+		coord = [stopDict[start_id]['name'], stopDict[start_id]['lat'], stopDict[start_id]['lng']]
+		return render_template("go.html", coord = coord,  same = True)
 	else:
 		path_id,bus_path,totalDur = dijkstra(start_id, end_id)
 		mins = totalDur // 60
 		secs = totalDur % 60
-		return render_template("go.html", start = start_name, end = end_name, mins = mins, secs = secs, path_id = path_id, bus_path = bus_path, same = False)
+		stop_coord = []
+		for stop in path_id:
+			stop_coord.append([stopDict[stop]['name'],stopDict[stop]['lat'], stopDict[stop]['lng']])
+		return render_template("go.html", mins = mins, secs = secs, stop_coord = stop_coord, bus_path = bus_path, same = False)
