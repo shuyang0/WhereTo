78d43be24a48611bda8eadd3176cbde41ee0f066
Shuyang <l.shuyang98@gmail.com>
2020-07-29 20:14:09 +0800

    implement basic combined dijkstra

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask import Flask, session, render_template, request, json
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from dijkstra import readData, dijkstra_bus, dijkstra_walk, stopDict, routeDict
+from dijkstra import readData, dijkstra_bus, dijkstra_walk, stopDict, routeDict, nodeDict
 
 app = Flask(__name__)
 
